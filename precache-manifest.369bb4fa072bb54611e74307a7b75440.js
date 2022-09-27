ee3bacf715b4a4ddaaf533adc9af699863602829
DarkFrog <demennikov.ivan@gmail.com>
2022-09-27 14:35:02 +0000

    Updates

---
+++ precache-manifest.369bb4fa072bb54611e74307a7b75440.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b41022c030c97e5224bf",
+    "url": "/DashboardReactReduxToolkit/static/css/main.d9224275.chunk.css"
+  },
+  {
+    "revision": "b41022c030c97e5224bf",
+    "url": "/DashboardReactReduxToolkit/static/js/main.b41022c0.chunk.js"
+  },
+  {
+    "revision": "e4d34f23597f50060fe5",
+    "url": "/DashboardReactReduxToolkit/static/js/1.e4d34f23.chunk.js"
+  },
+  {
+    "revision": "d225b16d11817f44b1c7",
+    "url": "/DashboardReactReduxToolkit/static/js/runtime~main.d225b16d.js"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/DashboardReactReduxToolkit/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "/DashboardReactReduxToolkit/static/media/bg-header-mobile.6b5ff5a0.svg"
+  },
+  {
+    "revision": "ae67c678a143e6c6b913629f3d3b55e9",
+    "url": "/DashboardReactReduxToolkit/static/media/icon-remove.ae67c678.svg"
+  },
+  {
+    "revision": "095706729536dca0fc6f41f1b907c502",
+    "url": "/DashboardReactReduxToolkit/index.html"
+  }
+];
